47c211dc3e1e6b6d810fd60d1c8ec03b9e00a606
Gabriel Richter Reynoso <garey91@gmail.com>
2024-11-20 15:40:43 +0100

    styling

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,18 +1,15 @@
 
 let Pokemon = ({name,sprites,types,height,weight}) => {
 
-
     return (
 
-        <>
-
-            <h3>{name}</h3>
+        <div className="pokemon-card">
             <img src={sprites.front_default}/>
+            <h4>{name}</h4>
             <h4>Type: {types[0].type.name}</h4>
             <h4>Height: {height}</h4>
             <h4>Weight: {weight}</h4>
-    
-        </>
+        </div>
     )
 }
 
