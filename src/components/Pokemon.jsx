a160848dccbbb2c55e79b19892323f86cbdf6c7c
Gabriel Richter Reynoso <garey91@gmail.com>
2024-11-28 21:42:23 +0100

    last changes

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -7,11 +7,11 @@ let Pokemon = ({ name, sprites, types, height, weight, toggleShow }) => {
                 <div className="image-div">
                     <img src={sprites.front_default} />
                 </div>
-                    <p className="name">{name}</p>
                 <div className="info-div">
+                    <p className="name">{name}</p>
                     <p>Type: {types[0].type.name}</p>
-                    <p>Height: {height}</p>
-                    <p>Weight: {weight}</p>
+                    <p>Height: {height}"</p>
+                    <p>Weight: {weight}lbs</p>
                 </div>
             </div>
             <button onClick={toggleShow}>Back</button>
--- src/pages/IntroPage.jsx
+++ src/pages/IntroPage.jsx
@@ -1,16 +1,17 @@
-import { Link } from 'react-router-dom'
 import image from '../images/pokeball.png'
 import image1 from '../images/7.png'
+import { useNavigate } from 'react-router-dom'
 
 const IntroPage = () => {
 
+    let navigate = useNavigate();
+
     return (
         <div className='container'>
-            {/* <h1>Pokemon App</h1> */}
             <img src={image1} alt="" className='titleimage'/>
-            <img src={image} className='pokeball'/>
             <p>- Get info about a certain Pokémon</p>
-            <button><Link to="/choosepokemon">Get Started</Link></button>
+            <img src={image} className='pokeball'/>
+            <button onClick={() => navigate('/choosepokemon')}>Get Started</button>
         </div>
     )
 }
